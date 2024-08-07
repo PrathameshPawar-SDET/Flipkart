158fdfe82fbb710f2dbf235a06e6526755a56cf2
PrathameshPawar-SDET <pawarprathamesh62@gmail.com>
2024-08-08 00:00:06 +0530

    Testcase01 changes

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,32 +1,74 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.security.PrivateKey;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
 
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
-    ChromeDriver driver;
+     private WebDriver driver;
+     private WebDriverWait wait;
+
+
+     @FindBy(xpath = "//input[contains(@placeholder,'Search for Products')]")
+     private WebElement searchElement;
+
+     @FindBy(xpath = "//button[contains(@title,'Search for Products')]")
+     private  WebElement SearchButton;
+
+     @FindBy(xpath = "//div[text()='Popularity']")
+     private  WebElement popularity;
+
+     @FindBy(xpath = "//span[contains(@id,'productRating')]")
+     private  List<WebElement> rating;
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
 
-     
+    @Test
+    public void testCase01(){
+        System.out.println("Start with testcase 01");
+        driver.get("https://www.flipkart.com/");
+
+        Wrappers.sendkeys(driver,searchElement,"Washing Machine");
+        Wrappers.click(driver,SearchButton);
+        Wrappers.click(driver, popularity);
+        int count = Wrappers.count(driver, rating);
+
+        System.out.println("Count of items with rating less than or equal to 4 stars: "+count);
+
+
+
+
+
+
+
+    }
+
+
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
@@ -36,7 +78,7 @@ public class TestCases {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+        WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
@@ -51,13 +93,14 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver,10), this);
     }
 
     @AfterTest
     public void endTest()
     {
-        driver.close();
-        driver.quit();
+//        driver.close();
+//        driver.quit();
 
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -11,12 +11,75 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
 import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    WebDriver driver;
+
+    public static boolean click(WebDriver driver, WebElement element){
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            Actions act = new Actions(driver);
+            act.moveToElement(element);
+            Thread.sleep(5000);
+            act.click(element).perform();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            try{
+                element.click();
+                return true;
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean sendkeys(WebDriver driver, WebElement inputbox, String keystosend){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(inputbox));
+        try {
+            inputbox.sendKeys(keystosend);
+            return true;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
+
+    public static int count(WebDriver driver, List<WebElement> Ratings){
+
+        int count =0;
+        for (WebElement rating : Ratings){
+            try{
+                double ratingvalue =Double.parseDouble(rating.getText().split(" ")[0]);
+                if(ratingvalue<=4.0){
+                    count++;
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+        return count;
+    }
+
+
 }
